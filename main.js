3a2efda80ccee62df5f9939d273e234df8896396
Takahiro FUKUYAMA <tatatakakaka0805@gmail.com>
2019-05-10 22:39:36 +0900

    update

--- main.js
+++ main.js
@@ -1,65 +1,94 @@
 var worksinfo = [
   {
-    "id": "connecte",
-    "name": "connecte",
-    "photo_num": 2,
-    "movie_num": 1,
-    "svg": true,
-    "year": 2018,
-    "info":["留学コンパスは、「留学したいけど何から始めたらいいのかわからない」といった悩みがある留学希望者を後押しするサービスとして、留学エージェント各社一押しの留学プランを、留学初心者でも一括比較できる留学体験の比較できる。関わってきて3度目のリニューアルで、各コンテンツの見せ方のデザインや検索のUXの向上を行った。",
-      "今までには会社の価値定義から、ブランディング。ペルソナの設定や、カスタマージャーニーマップの作成。現在サービスの全ページのUIデザインを行なっている。"]
-    },
-  {
     "id": "compass",
-    "name": "ryugakucompass",
-    "photo_num": 2,
-    "svg": true,
-    "year": 2018,
-    "client_url": "https://ryugakucompass.com",
-    "info":["留学コンパスは、「留学したいけど何から始めたらいいのかわからない」といった悩みがある留学希望者を後押しするサービスとして、留学エージェント各社一押しの留学プランを、留学初心者でも一括比較できる留学体験の比較できる。関わってきて3度目のリニューアルで、各コンテンツの見せ方のデザインや検索のUXの向上を行った。",
-      "今までにはワークショップを開催しながら、会社の価値定義、ブランディング。ペルソナの設定や、カスタマージャーニーマップの作成を行ってきた。<br>また、現在留学コンパスのサービスのほぼ全てのページのUIデザインや、機能の検証、改善を行なっている。"]
-    },
-  {
-    "id": "tritty",
-    "name": "tritty",
-    "photo_num": 2,
-    "svg": true,
-    "year": 2017,
-    "info":["TRITTYは、旅先や出かけた先で使える観光情報を提供するアプリケーションである。ユーザが観光をするときにあるニーズとして、情報の新鮮さやニッチさ、その地域特有の情報、位置情報、イメージが湧きやすいものであるか、体験者の声、旅の記録ができるというのが、観光庁などの調査のデータから示されている。従来の観光情報を提供するものは、写真やその場所の情報などがまとめられてはいるが、欲しい情報に行き着くまでの導線が複雑であったり、自分の現在地からその情報までの現実での距離感がわかりにくい場合がある。地図と写真がリンクして表示されるように画面を設計した。このことにより、現在地と情報が一目でわかるようになり、地図に情報を置くことにより、情報の流動性を低くした。",
-    "ユーザーが欲しい情報に行き着くまでの導線が複雑であり、現在地からその情報に対する現実的な距離感がわかりにくい場合があった。観光庁などの調査データも調べ、それらの問題点を解決しユーザーに新たな体験を提供する。"]
+    "date": "2018/05~2019/03",
+    "copy": "調べ方がわからなくても<br>行きたい留学が見つかる",
+    "overview": "留学コンパスは、「留学したいけど何から始めたらいいのかわからない」といった悩みがある留学希望者を後押しするサービスとして、留学エージェント各社一押しの留学プランを、留学初心者でも一括比較できる留学体験の比較できるサービス。",
+    "job": "UI/UXデザイン（スマホ、PC）、広告制作",
+    "tools": "Adobe XD/Photoshop/Illustrator/PremirePro/sketch",
+    "url": "https://ryugakucompass.com",
+    "title": [
+      "ユーザーの課題",
+      "サービスの利用体験設計",
+      "UIへの落とし込み",
+      "CVを上げるための施策、ナーチャリング施策",
+      "その他の制作物"
+    ],
+    "text": [
+      "留学コンパスでは、留学に興味を持つ人を利用者とし、「留学したい」と思って調べ回っても、結局自分はどんな留学をしたらいいのかわからず、はっきりと行きたい留学像を持てないことにより、具体的なイメージを持って留学エージェントとのカウンセリングに移れず、留学に行くという意思決定をしにくいことを課題として設定した。",
+      "最初に簡単な質問に答えるだけで、具体的な情報が載っている自分の希望に沿った留学プランが出てくるようにした。それらのプランを比較、検討しながら自分が留学までのステップの中でどの位置にいるのかを確認できるようにした。その中で、留学エージェントへの問い合わせが必要なステップの人たちは、サービス上で留学エージェントとマッチング。さらに、その場でメッセージを送ることができるようにした。",
+      "2000ほどの留学プランがあるため、比較のページでは、シームレスに様々な留学の仕方による留学プランの違いを比較できるようにした。質問から得られた情報によりその人にあった留学の仕方をタブとして設定し、横スクロールで各タブの切り替えで素早く比較できるようにした。また、こちらからのレコメンドのタブを設定することにより、利用者が迷った時に選びやすいようにした。"
+    ],
+    "photo_num": 5,
+    "info_data":[
+      1,1,1,1,1
+    ]
   },
   {
-    "id": "mylife",
-    "name": "mylife",
-    "photo_num": 1,
-    "svg": true,
-    "year": 2018,
-    "info":["グラフィックの授業で、自分の好きなものについてのポスターを制作する課題で制作した。自分の尊敬するアーティストであるZORNの1番好きな曲「My life」の世界を表現した。潰れた空き缶でカレンダーを表現しており、繰り返しの日々の中でも小さな幸せや輝ける日があるということを表した。"]
-  }
+    "id": "connecte",
+    "date": "2018/04~2018/08",
+    "copy": "照れワーカーの恋愛を<br>後押しするデバイス",
+    "overview": "テレワーカーを対象にした、初対面の異性同士のコミュニケーションを促進するIotデバイスを開発した。恋人つなぎという一見敷居の高い接触をデバイスを通して行うことで、その敷居を下げ恋人つなぎでしか得られないドキドキ感を提供するというデバイス。",
+    "job": "コンセプトメイク、Aruduino制御",
+    "tools": "Aruduino IDE/firebase",
+    "title": [
+      "出会い、コミュニケーション、ドキドキ感の創出",
+      "システム構成",
+      "コンセプト動画"
+    ],
+    "text": [
+      "自宅以外で仕事をするテレワーカーはテレワーカー 全体の中でも過半数存在し、ユーザは多く存在することが容易に予測できる。よって、テレワーカーの出会いの場としてコワーキングスペースに注目した。また、デバイスによって話しかける建前を作り、テレワーカーの会話が生まれやすい状況を作った。次に相互作用の生じるデバイスで擬似恋人繋ぎをすることで初対面の二人の間にドキドキ感を生み出した。",
+      "手形デバイスとライトを1セットで扱いライトと手の間は有線で接続。デバイス間の通信はwifiモジュールを使用し、データベースに値を投げて通信している。手の裏にある超音波センサーで、指までの距離を測りそれを実際にサーボモーターが指を動かす角度に変換している。"
+    ],
+    "photo_num": 3,
+    "movie_num": 2,
+    "movie_url": [
+      '<iframe class="movie" src="https://www.youtube.com/embed/feT2pkiEoqw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      '<iframe class="movie" src="https://www.youtube.com/embed/ypc281XRjKk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    ],
+    "info_data":[
+      2,1,2
+    ]
+  },
+    {
+      "id": "tritty",
+      "date": "2017/07~2018/02",
+      "copy": "旅先での地域特有の”新鮮な情報”を<br>直感的に入手し、辿り着きやすくする",
+      "overview": "TRITTYは、旅先や出かけた先で使える観光情報を提供するアプリケーションである。地図と写真がリンクして表示されることにより、実空間においてその観光情報との距離が測りやすく直感的に情報を探せる。",
+      "job": "卒業制作、UI/UXデザイン",
+      "tools": "Adobe XD/Photoshop/Illustrator",
+      "title": [
+        "観光情報に対する観光客のニーズ調査、分析<br>ニーズを基にした既存のツール分析",
+        "ペルソナの設定、UXマップの作成",
+        "UIデザイン"
+      ],
+      "text": [
+        "国土交通省のデータなどにより、観光におけるニーズを洗い出した。その後、それらを分析しユーザーが持つ不満と掛け合わせて下記のようなニーズを仮定した。また、旅行者が観光情報を得る主なサービスをピックアップし、それらが上記に挙げた7つのニーズを満たしているのかを分析した。そして求められる機能を洗い出した。"
+      ],
+      "photo_num": 4,
+      "info_data":[
+        2,1,1
+      ]
+    },
+    {
+      "id": "mylife",
+      "date": "2018/07",
+      "copy": "ZORNの「My life」",
+      "overview": "グラフィックの授業で、自分の好きなものについてのポスターを制作する課題で制作した。自分の尊敬するアーティストであるZORNの1番好きな曲「My life」の世界を表現した。潰れた空き缶でカレンダーを表現しており、繰り返しの日々の中でも小さな幸せや輝ける日があるということを表した。",
+      "job": "授業課題",
+      "tools": "Photoshop/Illustrator"
+    }
 ]
 
-var t;
-var w;
-var h;
+var header = document.getElementById("workHeader");
+var main = document.getElementById("workView");
 
 window.onload = function onload(){
-  var contentInfo = document.getElementById("contentInfo");
-  var contentPhoto = document.getElementById("contentPhoto");
-  w = document.documentElement.clientWidth;
-  fitWindow();
   param = GetQueryString();
   if( param ) {
     var data = getData(param);
     view(data);
   }
-}
-window.onresize = function() {
-  w = document.documentElement.clientWidth;
-  fitWindow();
-}
-
-function fitWindow() {
-    document.body.style.width = w + "px";
 }
 
 function GetQueryString() {
@@ -79,50 +108,55 @@ function getData(param) {
 
 function view(data) {
 
-  contentVer.insertAdjacentHTML('beforeend','<img class="rightText" src="./data/works/' + data.id + '/' + data.name + '-ver' + '.svg">');
-
-  if ( data.svg ) {
-    contentInfo.insertAdjacentHTML('beforeend','<img class="infoImg" src="./data/works/' + data.id + '/' + data.id + '-logo' + '.svg">');
+  header.insertAdjacentHTML('beforeend','<img class="headerImg" src="./data/works/' + data.id + '/' + data.id + '_header.png">');
+  main.insertAdjacentHTML('beforeend','<img class="contentLogo" src="./data/works/' + data.id + '/' + data.id + '-logo' + '.svg">');
+  main.insertAdjacentHTML('beforeend','<p class="date">' + data.date + '</p>');
+  main.insertAdjacentHTML('beforeend','<p class="copy">' + data.copy + '</p>');
+  main.insertAdjacentHTML('beforeend','<p class="overview">' + data.overview + '</p>');
+  main.insertAdjacentHTML('beforeend','<p class="subtitle">担当</p>');
+  main.insertAdjacentHTML('beforeend','<p class="job">' + data.job + '</p>');
+  main.insertAdjacentHTML('beforeend','<p class="subtitle">使用ツール</p>');
+  main.insertAdjacentHTML('beforeend','<p class="tools">' + data.tools + '</p>');
+  if(data.url) {
+    main.insertAdjacentHTML('beforeend','<p class="subtitle">URL</p>');
+    main.insertAdjacentHTML('beforeend','<p class="tools">' + data.url + '</p>');
   }
 
-  for(var i = 0; i < data.photo_num; i++){
-    contentInfo.insertAdjacentHTML('beforeend','<img class="contentImg" src="./data/works/' + data.id + '/' + data.id + '_' + [i] + '.png">');
-    contentInfo.insertAdjacentHTML('beforeend','<p class="info">' + data.info[i] + '</p>');
-  }
-  if( data.movie_num ) {
-    for(var i = 0; i < data.movie_num; i++){
-      contentPhoto.insertAdjacentHTML('beforeend','<video controls class="contentMovie" src="./data/works/' + data.id + '/' + data.id + '_' + [i] + '.mp4"></video>');
+  for(var i = 0; i < data.title.length; i++){
+    main.insertAdjacentHTML('beforeend','<p class="title">' + data.title[i] + '</p>');
+    if(i < data.text.length){
+      main.insertAdjacentHTML('beforeend','<p class="text">' + data.text[i] + '</p>');
+    }
+
+    if(data.info_data[i] !== 0) {
+      var val = calc(data.info_data, i);
+      if( val < data.photo_num ) {
+        for(var s = 0; s < data.info_data[i]; s++) {
+          var num = s + val;
+          photo_load(data.id, num);
+        }
+      } else if( data.movie_url !==0 ){
+        movie_load(2, data.movie_url);
+      }
     }
   }
-  contentInfo.insertAdjacentHTML('beforeend','<p class="year">' + data.year + '</p>');
-  if ( data.client_url ) {
-    contentInfo.insertAdjacentHTML('beforeend','<p class="client_url"><a href="' + data.client_url + '">' + data.client_url + '</a></p>');
-  }
 
 }
 
-var menu = document.getElementById("menu");
-var menuSp = document.getElementById("menuDrower");
+function calc(data, a) {
+  var val = 0;
+  for (var i = 0; i < a; i++){
+    val += data[i]
+  }
+  return val;
+}
 
-var pos = 0;
-
-function drower() {
-  switch(pos){
-    case 0 :
-      menu.style.transform = "translateX(" + pos + "px)";
-      pos = -200;
-      menuSp.classList.add('active');
-      break;
-    case -200:
-      menu.style.transform = "translateX(" + pos + "px)";
-      pos = 0;
-      menuSp.classList.remove('active');
-      break;
+function movie_load(number, url) {
+  for (var t = 0; t < number; t++) {
+    main.insertAdjacentHTML('beforeend',url[t]);
   }
 }
 
-function menuClose() {
-  menu.style.transform = "translateX(-200px)";
-  open.style.display = "block";
-  close.style.display = "none";
+function photo_load(id, number) {
+  main.insertAdjacentHTML('beforeend','<img class="workImg" src="./data/works/' + id + '/' + id + '_' + number + '.png">');
 }
