715262232cef8e00de1535bbf44cb02fad696a8b
Takahiro FUKUYAMA <tatatakakaka0805@gmail.com>
2019-06-22 02:05:34 +0900

    fix

---
+++ script.js
@@ -0,0 +1,41 @@
+$(function() {
+  var h = $(window).height();
+  $('#main-contents').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+});
+
+$(window).load(function () {
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+  $('#main-contents').css('display', 'block');
+});
+
+// $("html,body").animate({scrollTop:$('#works-id').offset().top});
+
+$('#scroll300').click(function() {
+    var about = $('#about-id').offset().top;
+
+    $('html, body').animate({scrollTop:about});
+
+})
+
+$('#top-menu').click(function() {
+    var topmenu = $('#top-id').offset().top;
+
+    $('html, body').animate({scrollTop:topmenu});
+
+})
+
+$('#about-menu').click(function() {
+    var aboutmenu = $('#about-id').offset().top;
+
+    $('html, body').animate({scrollTop:aboutmenu});
+
+})
+
+$('#works-menu').click(function() {
+    var worksmenu = $('#works-id').offset().top;
+
+    $('html, body').animate({scrollTop:worksmenu});
+
+})
