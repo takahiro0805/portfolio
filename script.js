a1f400b05128cae883dfaf94273a2dfeb61c60aa
Takahiro FUKUYAMA <tatatakakaka0805@gmail.com>
2019-06-29 03:29:34 +0900

    fix

--- script.js
+++ script.js
@@ -40,3 +40,38 @@ $('#works-menu').click(function() {
     $('html, body').animate({scrollTop:worksmenu});
 
 })
+
+currentItem = $(".current-item");
+if (currentItem[0]) {
+  currentItem.css({
+    "width": currentItem.width(),
+    "left": currentItem.position().left
+  });
+}
+
+$("#header a").hover(
+  // ホバー時
+  function(){
+    // ホバーしているメニューの幅と左位置に変更
+    $("#slide-line").css({
+      "width": $(this).width(),
+      "left": $(this).position().left
+    });
+  },
+  // ホバーが外れたとき
+  function(){
+    // カレントメニューがある場合
+    if (currentItem[0]) {
+      // カレントメニューに下線を戻す
+      $("#slide-line").css({
+        "width": currentItem.width(),
+        "left": currentItem.position().left
+      });
+    }
+    // カレントメニューがない場合
+    else {
+      // 下線を消す
+      $("#slide-line").width(0);
+    }
+  }
+ );
